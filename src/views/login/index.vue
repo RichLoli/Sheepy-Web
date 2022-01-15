328b9fe5ffb460f2c1c838b1312c6c3551bb0222
Aya <loligafeo@gmail.com>
2022-01-15 18:12:07 +0800

    登录页优化

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,60 +3,7 @@
     <div class="m">
       <div id="login-banner">
         <div id="login-maintainer">
-          <div id="login-info">
-            <div>
-              <span>SHEEPY</span>
-            </div>
-          </div>
-          <div id="login-card">
-            <div class="login-title">
-              <span>账户登录</span>
-              <el-divider
-                :style="{ 'background-color': '#cbcbcb' }"
-              ></el-divider>
-            </div>
 
-            <div class="login-form">
-              <el-form
-                :model="user"
-                :label-position="labelPosition"
-                :rules="rules"
-                ref="userLogin"
-                label-width="120px"
-              >
-                <el-form-item label="" prop="username">
-                  <el-input
-                    v-model="user.username"
-                    placeholder="请输入用户名/手机号"
-                  ></el-input>
-                </el-form-item>
-                <el-form-item label="" prop="password">
-                  <el-input
-                    v-model="user.password"
-                    type="password"
-                    autocomplete="off"
-                    placeholder="请输入密码"
-                    prop="password"
-                  ></el-input>
-                </el-form-item>
-                <el-form-item> </el-form-item>
-                <el-form-item>
-                  <el-button
-                    class="button-full"
-                    type="primary"
-                    @click="handleLogin"
-                    >立即登录</el-button
-                  >
-                </el-form-item>
-              </el-form>
-            </div>
-            <div>
-              <div class="login-tips">
-                <router-link to="/login">忘记密码？</router-link>
-                <router-link to="/login">没有账户？立即注册></router-link>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
