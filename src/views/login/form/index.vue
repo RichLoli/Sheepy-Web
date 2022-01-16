0a3cd65b823c251de40dfaaef96738fcce171ce2
Aya <loligafeo@gmail.com>
2022-01-17 00:18:46 +0800

    登录组件优化

--- src/views/login/form/index.vue
+++ src/views/login/form/index.vue
@@ -53,10 +53,129 @@
 </template>
 
 <script>
+import { reactive, ref, unref, defineComponent } from "vue";
 export default {
+  setup() {
+    const userLogin = ref(null);
 
-}
+    const user = reactive({
+      username: "",
+      password: "",
+    });
+
+    const rules = reactive({
+      username: [
+        {
+          required: true,
+          message: "请输入用户名",
+          trigger: "blur",
+        },
+        {
+          min: 3,
+          max: 20,
+          message: "用户名应为 3-20 个字符",
+          trigger: "blur",
+        },
+      ],
+      password: [
+        {
+          required: true,
+          message: "请输入密码",
+          trigger: "blur",
+        },
+        {
+          min: 6,
+          message: "密码应不少于6位",
+          trigger: "blur",
+        },
+      ],
+    });
+
+    const handleLogin = async () => {
+      const form = unref(userLogin);
+      if (!form) return;
+      form.validate((valid) => {
+        if (valid) {
+          console.log("submit!");
+          this.$cookies.set("token", "admin");
+          // this.$router.push("/");
+        } else {
+          console.log("error submit!");
+          return false;
+        }
+      });
+    };
+
+    return {
+      user,
+      rules,
+      userLogin,
+      handleLogin,
+    };
+  },
+  data() {
+    return {
+      labelPosition: "top",
+    };
+  },
+};
 </script>
 
 <style>
+#login-board {
+  display: flex;
+}
+
+#login-info {
+  height: 310px;
+  width: 165px;
+  padding: 25px;
+  background-color: rgb(235, 235, 235);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+}
+
+#login-info span {
+  font-size: 2.5em;
+  font-weight: bold;
+  margin-right: 100px;
+  color: #499edb;
+  font-family: "微软雅黑";
+}
+
+#login-card {
+  height: 310px;
+  width: 340px;
+  padding: 25px;
+  background-color: #fff;
+  box-shadow: rgb(0 0 0 / 15%) 0px 2px 15px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+#login-card > div {
+  margin-bottom: 15px;
+}
+
+.login-title {
+  font-size: 1.3em;
+  font-weight: bolder;
+  color: #277cd5;
+}
+
+.login-tips {
+  font-size: 0.5em;
+  display: flex;
+  justify-content: space-between;
+}
+
+.login-tips > a {
+  color: rgb(114, 114, 114);
+}
+
+.login-form label {
+  padding: 0px !important;
+}
 </style>
